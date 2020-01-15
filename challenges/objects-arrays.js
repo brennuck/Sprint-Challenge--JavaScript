15530564913e7c9ac9da53de152a4c0e7ad0020c
Brennon Nuckols <47544816+brennuck@users.noreply.github.com>
2020-01-15 11:09:16 -0700

    Revert "Sprint-Challenge--Javascript"

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -15,44 +15,20 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-const tyrannosaurus = {
-  name: 'tyrannosaurus',
-  diet: 'carnivorous',
-  weight: '7000kg',
-  length: '12m',
-  period: 'Late Cretaceous',
-  roar: function() {
-    return "RAWERSRARARWERSARARARRRR!"
-  }
-}
-const stegosaurus = {
-  name: 'stegosaurus',
-  diet: 'herbivorous',
-  weight: '2000kg',
-  length: '9m',
-  period: 'Late Jurassic'
-}
-const velociraptor = {
-  name: 'velociraptor',
-  diet: 'carnivorous',
-  weight: '15kg',
-  length: '1.8m',
-  period: 'Late Cretaceous'
-}
-console.log(tyrannosaurus.weight);
+console.log();
 
 // What was the diet of a velociraptor?
-console.log(velociraptor.diet);
+console.log();
 
 // How long was a stegosaurus?
-console.log(stegosaurus.length);
+console.log();
 
 // What time period did tyrannosaurus live in?
-console.log(tyrannosaurus.period);
+console.log();
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log(tyrannosaurus.roar());
+console.log();
 
 
 // ==== Arrays ====
@@ -75,10 +51,8 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = graduates.map((state) => {
-  return state.university;
-});
-console.log(universities.sort());
+const universities = [];
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -86,13 +60,11 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = graduates.map((state) => {
-  return `${state.first_name} ${state.email}`;
-});
+const contactInfo = [];
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = graduates.filter(grad => grad.university.includes("Uni"));
+const unisWithUni = [];
 console.log(unisWithUni);
 
 
@@ -119,9 +91,6 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
-zooAnimals.forEach((names) => {
-  return displayNames.push(`${names.animal_name}, ${names.scientific_name}`)
-})
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -130,9 +99,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = zooAnimals.map((state) => {
-  return `${state.animal_name.toLowerCase()}`
-})
+const lowCaseAnimalNames = [];
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -140,9 +107,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = zooAnimals.filter((state) => {
-  return state.population < 5;
-});
+const lowPopulationAnimals = [];
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -150,13 +115,13 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = zooAnimals.reduce((total, state) => {
-  return total += state.population;
-})
+const populationTotal = 0;
 console.log(populationTotal);
 
 
 /*
+
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+
 */
-// Done
+
